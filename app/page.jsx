800402d65e2320dadd555179b01e182587e29fcb
timenotspace <timenotspace.eth@gmail.com>
2023-08-24 14:25:16 -0400

    meeping up some of those build issues

--- app/page.jsx
+++ app/page.jsx
@@ -7,8 +7,6 @@ import { StarfieldCanvas } from '@/components/StarfieldCanvas'
 import { useState } from 'react';
 import Link from 'next/link';
 
-
-
 const Logo = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Logo), { ssr: false })
 const Dog = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Dog), { ssr: false })
 const Duck = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Duck), { ssr: false })
@@ -29,36 +27,27 @@ const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.
 })
 const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
 
-
-
 export default function Page() {
-
   const [showRedOrb, setShowRedOrb] = useState(false);
-
   const handleViewClick = () => {
     setShowRedOrb(true);
   };
 
-
-
   return (
     <>
       <StarfieldCanvas />
-
-      <div className="navigation">
+      <div className="mb-4">
         <Link href="/TestStarfield">Go to Test Starfield</Link>
       </div>
-
-      <div className='mx-auto flex w-full flex-col flex-wrap items-center bg-transparent md:flex-row  lg:w-4/5'>
-        {/* jumbo */}
+      <div className='mx-auto flex w-full flex-col flex-wrap items-center bg-transparent md:flex-row lg:w-4/5'>
         <div className='flex w-full flex-col items-start justify-center p-12 text-center bg-transparent md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'> 🌺euphoric experimental 3D development </p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>timephor ⚘</h1>
-          <p className="mb-8 text-xs leading-normal"> TEST GARDEN 4 TAIGA-ZOKU & FRIENDS </p>
-          <p className="mb-8 text-sm leading-normal"> [pardon the timenotspace] &apos;🏗️&apos; </p>
-
+          <p className='w-full uppercase mb-4'>ｅｘｐｅｒｉｍｅｎｔａｌ ｄｅｖ</p>
+          <h1 className='my-4 text-5xl font-bold leading-tight'>⚘EᴜᴘʜᴏʀɪᴄWRWC </h1>
+          <p className="mb-8 text-xs leading-normal">5D TEST GARDEN 0.1</p>
+          <p className="mb-8 text-xs leading-normal">~ TAIGA-ZOKU & FRIENDS ~</p>
+          <p className="mb-8 text-sm leading-normal">[ ᴘᴀʀᴅᴏɴ sᴘᴀᴄᴇ ᴄᴏɴsᴛʀᴜᴄᴛɪᴏɴ ] 🏗️</p>
         </div>
-        <div className='w-full text-center  bg-transparent md:w-3/5'>
+        <div className='w-full text-center bg-transparent md:w-3/5'>
           <View className='flex h-96 w-full flex-col items-center justify-center' onClick={handleViewClick}>
             <Suspense fallback={null}>
               {showRedOrb && <RedOrb />}
@@ -68,19 +57,13 @@ export default function Page() {
           </View>
         </div>
       </div>
-
-
-      <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 bg-transparent md:flex-row  lg:w-4/5'>
-
-        {/* first row */}
-
+      <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 bg-transparent md:flex-row lg:w-4/5'>
         <div className='relative h-48 w-full py-6 sm:w-1/2 bg-transparent md:my-12 md:mb-40'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-cyan-300'>Building new types of interaction</h2>
-          <p className='mb-8 text-cyan-300'>Explore the 3D scenes...</p>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-cyan-300'>interaction design buildout</h2>
+          <p className='mb-8 text-cyan-300'>multiple interactive 3D scenes on each page</p>
         </div>
-
         <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 bg-transparent md:mb-40'>
-          <View orbit className='relative h-full  sm:h-48 sm:w-full' onClick={handleViewClick}>
+          <View orbit className='relative h-full sm:h-48 sm:w-full' onClick={handleViewClick}>
             <Suspense fallback={null}>
               {showRedOrb && <RedOrb />}
               <Dog scale={2} position={[0, -1.6, 0]} rotation={[0.0, -0.3, 0]} />
@@ -88,13 +71,8 @@ export default function Page() {
             </Suspense>
           </View>
         </div>
-
-        {/* second row */}
-
         <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 bg-transparent md:mb-40'>
-          {/* <View orbit className='relative h-full animate-bounce sm:h-48 sm:w-full' onClick={handleViewClick}> */}
           <View orbit className='relative h-full sm:h-48 sm:w-full' onClick={handleViewClick}>
-
             <Suspense fallback={null}>
               {showRedOrb && <RedOrb />}
               <Duck route='/blob' scale={2} position={[0, -1.6, 0]} />
@@ -102,21 +80,12 @@ export default function Page() {
             </Suspense>
           </View>
         </div>
-
         <div className='w-full p-6 bg-transparent sm:w-1/2'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-cyan-300'>Paradigms that don't exist elsewhere</h2>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-cyan-300'>combining pieces in new ways</h2>
           <p className='mb-8 text-cyan-300'>
-            3D Divs are rendered through the View component.
-
-            We tie a view to a tracking div which then controls the position and bounds of the viewport.
-
-            This allows us to have multiple views with a single, performant canvas.
-
-            These views will follow their tracking elements, scroll along, resize, etc.
+            immersive 3D spaces , music , digital objects , simulated physics , new types of shared experiences by WRWC development in collaboration with 🌺Euphoric
           </p>
         </div>
-
-
       </div>
     </>
   )
